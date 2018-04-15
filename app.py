5ac7d3960ac4fbf4317cb43060054450a6aa225b
Jonathan Feenstra <jonathan.feenstra@upcmail.nl>
2018-04-15 10:34:49 +0200

    Changed project to Flask

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+#app.debug = True
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run()
